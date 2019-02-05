f7c6ab5415102d0cbcd924553ba5f70ca30b3591
David Govea <govea.d@gmail.com>
2019-02-04 19:31:45 -0800

    refactor part1

--- aoc01/src/main.rs
+++ aoc01/src/main.rs
@@ -6,9 +6,15 @@ fn main() -> Result<(), Box<std::error::Error>> {
     let mut input = String::new();
     io::stdin().read_to_string(&mut input)?;
 
+    part1(&input);
+
+    Ok(())
+}
+
+fn part1(input: &str) -> Result<(), Box<std::error::Error>> {
+
     let frequency_shift: i32 = input.lines().map(|n| n.parse::<i32>().unwrap()).sum();
 
     writeln!(io::stdout(), "Even without units, I can tell this thingy has shifted by about {:?}!", frequency_shift)?;
-
     Ok(())
 }
