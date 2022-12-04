fdc124a21a6831aed2b6c24ee0f8521464deba20
David Govea <govea.d@gmail.com>
2022-12-04 02:15:40 -0500

    Finish 2

--- 2022/src/bin/02.rs
+++ 2022/src/bin/02.rs
@@ -7,9 +7,9 @@ enum Play {
 
 #[derive(Debug, PartialEq)]
 enum Result {
-    Win,
     Lose,
     Draw,
+    Win,
 }
 
 fn play_round(opponent_play: Play, my_play: Play) -> Result {
@@ -43,7 +43,7 @@ fn parse_play(c: &str) -> Play {
         "A" => Play::Rock,
         "B" => Play::Paper,
         "C" => Play::Scissors,
-        // My moves
+        // My moves (part 1 only)
         "X" => Play::Rock,
         "Y" => Play::Paper,
         "Z" => Play::Scissors,
@@ -52,7 +52,8 @@ fn parse_play(c: &str) -> Play {
 }
 
 pub fn part_one(input: &str) -> Option<u32> {
-    let total_score = input.lines()
+    let total_score = input
+        .lines()
         .map(|round| {
             // Such a hullabaloo to get the parsed plays!
             let [opponent_play, my_play]: [Play; 2] = round
@@ -71,8 +72,45 @@ pub fn part_one(input: &str) -> Option<u32> {
     Some(total_score)
 }
 
+fn parse_desired_result(r: &str) -> Result {
+    match r {
+        "X" => Result::Lose,
+        "Y" => Result::Draw,
+        "Z" => Result::Win,
+        _ => panic!("Invalid string: {}", r),
+    }
+}
+
+fn determine_play(opponent_play: Play, desired_result: Result) -> Play {
+    match (opponent_play as isize + desired_result as isize) % 3 {
+        0 => Play::Rock,
+        1 => Play::Paper,
+        2 => Play::Scissors,
+        _ => panic!("Invalid play"),
+    }
+}
+
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let total_score = input
+        .lines()
+        .map(|round| {
+            let [opponent_symbol, result_symbol]: [&str; 2] = round
+                .split_whitespace()
+                .collect::<Vec<&str>>()
+                .try_into()
+                .ok() // I trust my inputs <3
+                .unwrap();
+
+            let opponent_play = parse_play(opponent_symbol);
+            let desired_result = parse_desired_result(result_symbol);
+            let my_play = determine_play(opponent_play, desired_result);
+
+            let result = play_round(opponent_play, my_play);
+            score_round(my_play, result)
+        })
+        .sum();
+
+    Some(total_score)
 }
 
 fn main() {
@@ -94,6 +132,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let input = aoc2022::read_file("examples", 2);
-        assert_eq!(part_two(&input), None);
+        assert_eq!(part_two(&input), Some(12));
     }
 }
