c7eaa0c2b2217364b16af8ead9180f57ab90b70e
David Govea <govea.d@gmail.com>
2022-12-05 00:49:07 -0500

    4 pt 2

--- 2022/src/bin/04.rs
+++ 2022/src/bin/04.rs
@@ -1,26 +1,32 @@
 use std::ops::{Range, RangeInclusive};
 
+fn get_sorted_ranges(line: &str) -> [RangeInclusive<u32>; 2] {
+    let mut ranges = line
+        .split(',')
+        .map(|elf| {
+            let [lower, upper]: [u32; 2] = elf
+                .split('-')
+                .map(|n| n.parse::<u32>().unwrap())
+                .collect::<Vec<_>>()
+                .try_into()
+                .ok()
+                .unwrap();
+
+            lower..=upper
+        })
+        .collect::<Vec<_>>();
+
+    ranges.sort_by(|a, b| a.start().cmp(&b.start()).then(b.end().cmp(&a.end())));
+
+    ranges.try_into().ok().unwrap()
+}
+
 pub fn part_one(input: &str) -> Option<u32> {
     let mut contained_pairs = 0;
 
     for line in input.lines() {
-        let mut ranges = line
-            .split(',')
-            .map(|elf| {
-                let [lower, upper]: [u32; 2] = elf
-                    .split('-')
-                    .map(|n| n.parse::<u32>().unwrap())
-                    .collect::<Vec<_>>()
-                    .try_into()
-                    .ok()
-                    .unwrap();
-                lower..=upper
-            })
-            .collect::<Vec<_>>();
+        let [elf1, elf2] = get_sorted_ranges(line);
 
-        ranges.sort_by(|a, b| a.start().cmp(&b.start()).then(b.end().cmp(&a.end())));
-
-        let [elf1, elf2]: [RangeInclusive<u32>; 2] = ranges.try_into().ok().unwrap();
         if elf1.contains(&elf2.start()) && elf1.contains(&(elf2.end())) {
             contained_pairs += 1;
         }
@@ -29,7 +35,16 @@ pub fn part_one(input: &str) -> Option<u32> {
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let mut overlapping_pairs = 0;
+
+    for line in input.lines() {
+        let [elf1, elf2] = get_sorted_ranges(line);
+
+        if elf1.contains(&elf2.start()) {
+            overlapping_pairs += 1;
+        }
+    }
+    Some(overlapping_pairs)
 }
 
 fn main() {
@@ -51,6 +66,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let input = aoc2022::read_file("examples", 4);
-        assert_eq!(part_two(&input), None);
+        assert_eq!(part_two(&input), Some(4));
     }
 }
